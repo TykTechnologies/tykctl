7f4b8b95deb0a9194bfb98a97a73bbbe7e4651e6
keith wachira <keithwachira@gmail.com>
2022-08-31 07:56:24 +0300

    test generated sdk

--- ara-client-sdk/main.go
+++ ara-client-sdk/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"ara-client-sdk/swagger-gen"
+	"context"
+	"fmt"
+	"log"
+)
 
+func main() {
+	config := swagger.Configuration{
+		BasePath:      "http://localhost:4010",
+		DefaultHeader: make(map[string]string),
+	}
+	client := swagger.NewAPIClient(&config)
+	list, _, err := client.EntitlementsApi.AdminGetEnt(context.Background(), "ssss")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	log.Println("hello worked again")
+	fmt.Printf("%+v\n", list.Payload)
 }

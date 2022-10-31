c867e2f82ad023c5d23c4dbacc107a74e586dd5e
keith wachira <keithwachira@gmail.com>
2022-10-31 19:08:45 +0300

    add tests for validateFlagsAndCreateTeam

--- cmd/create_team_test.go
+++ cmd/create_team_test.go
@@ -70,6 +70,68 @@ func TestCreateTeam(t *testing.T) {
 		})
 	}
 }
+func TestValidateFlagsAndCreateTeam(t *testing.T) {
+	tests := []struct {
+		name                       string
+		ExpectedError              error
+		mockError                  error
+		mockHttpResponse           *http.Response
+		mockResponse               cloud.InlineResponse2011
+		numberOfTimeToCallMockFunc int
+		teamName                   string
+		org                        string
+		expectedTeamResponse       *cloud.Team
+	}{{
+		name:             "Test Success",
+		ExpectedError:    nil,
+		mockError:        nil,
+		mockHttpResponse: &http.Response{StatusCode: http.StatusCreated},
+		mockResponse: cloud.InlineResponse2011{
+			Error_:  "",
+			Payload: &generateTeams(1)[0],
+			Status:  "ok",
+		},
+		numberOfTimeToCallMockFunc: 1,
+		teamName:                   "team name",
+		org:                        "u4ieu47ueu",
+		expectedTeamResponse:       &cloud.Team{OID: "1", UID: "1"},
+	}, {
+		name:                       "Test org is required",
+		ExpectedError:              ErrorOrgRequired,
+		mockError:                  nil,
+		mockHttpResponse:           nil,
+		mockResponse:               cloud.InlineResponse2011{},
+		numberOfTimeToCallMockFunc: 0,
+		teamName:                   "",
+		org:                        "",
+		expectedTeamResponse:       nil,
+	},
+		{
+			name:                       "Test team name is required",
+			ExpectedError:              ErrorNameRequired,
+			mockError:                  nil,
+			mockHttpResponse:           nil,
+			mockResponse:               cloud.InlineResponse2011{},
+			numberOfTimeToCallMockFunc: 0,
+			teamName:                   "",
+			org:                        "4i57564j",
+			expectedTeamResponse:       nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			m := mock.NewMockCloudClient(ctrl)
+			m.EXPECT().CreateTeam(gomock.Any(), gomock.Any(), gomock.Any()).Times(tt.numberOfTimeToCallMockFunc).Return(tt.mockResponse, tt.mockHttpResponse, tt.mockError)
+			team, err := validateFlagsAndCreateTeam(context.Background(), m, tt.teamName, tt.org)
+			assert.Equal(t, tt.ExpectedError, err)
+			assert.Equal(t, tt.expectedTeamResponse, team)
+
+		})
+
+	}
+}
 func TestCreateTeamFlags(t *testing.T) {
 	cmd := NewCmd("test").WithFlagAdder(false, createTeamFlags).NoArgs(nil)
 	flags := []Flag{{

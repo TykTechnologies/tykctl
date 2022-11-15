6e6e97a64fe115ffb81ef00a311990d904100782
keith wachira <keithwachira@gmail.com>
2022-11-15 16:01:59 +0300

    add custom documentation

--- docs/docs.go
+++ docs/docs.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"bytes"
 	"github.com/TykTechnologies/tykctl/cloudcmd"
+	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 	"log"
+	"os"
 )
 
 func main() {
-	tykctl := cloudcmd.NewCloudCommand(nil)
-	err := doc.GenMarkdownTree(tykctl, "./")
+	f, err := os.OpenFile("docs.md", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
+	defer f.Close()
+	out := new(bytes.Buffer)
+	s := []*cobra.Command{cloudcmd.NewLoginCommand()}
+	for _, cmd := range s {
+		err := doc.GenMarkdown(cmd, out)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if _, err = f.Write(out.Bytes()); err != nil {
+			panic(err)
+		}
+	}
+
 }

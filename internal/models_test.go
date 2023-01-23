9651e8cf138cfac9e93a75e6c140dd50255a56ea
keith wachira <keithwachira@gmail.com>
2023-01-23 10:48:09 +0300

    add tests to our models

--- internal/models_test.go
+++ internal/models_test.go
@@ -5,27 +5,87 @@ import (
 	"testing"
 )
 
-func TestOrgInit_OrgInitToMap(t *testing.T) {
-	type fields struct {
-		Controller string
-		Org        string
-		Team       string
-	}
+func TestOrgInitOrgInitToMap(t *testing.T) {
+
 	tests := []struct {
-		name   string
-		fields fields
-		want   map[string]string
+		name    string
+		orgInit OrgInit
+		want    map[string]string
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Test has no org",
+			orgInit: OrgInit{
+				Controller: "https://tyk.io/eu-east",
+				Org:        "",
+				Team:       "My Team Here",
+			},
+			want: map[string]string{
+				"controller": "https://tyk.io/eu-east",
+				"team":       "My Team Here",
+			},
+		},
+		{
+			name: "Test Success",
+			orgInit: OrgInit{
+				Controller: "https://tyk.io/us-west",
+				Org:        "DX org",
+				Team:       "My Team",
+			},
+			want: map[string]string{"controller": "https://tyk.io/us-west",
+				"team": "My Team",
+				"org":  "DX org",
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			o := OrgInit{
-				Controller: tt.fields.Controller,
-				Org:        tt.fields.Org,
-				Team:       tt.fields.Team,
-			}
-			assert.Equalf(t, tt.want, o.OrgInitToMap(), "OrgInitToMap()")
+			assert.Equalf(t, tt.want, tt.orgInit.OrgInitToMap(), "OrgInitToMap()")
+		})
+	}
+}
+
+func TestRoleToMap(t *testing.T) {
+	tests := []struct {
+		name string
+		role Role
+		want map[string]string
+	}{
+
+		{
+			name: "Test has empty team",
+			role: Role{
+				Role:      "org_admin",
+				OrgID:     "89084736352",
+				TeamID:    "",
+				OrgName:   "Second Org",
+				TeamName:  "Chose Team",
+				AccountID: "94847472747",
+			},
+			want: map[string]string{
+				"role": "org_admin",
+				"org":  "89084736352",
+			},
+		},
+		{
+			name: "Test Success",
+			role: Role{
+				Role:      "team_member",
+				OrgID:     "56ui84736",
+				TeamID:    "jkituy6rt",
+				OrgName:   "My Org",
+				TeamName:  "New Team",
+				AccountID: "890o47373",
+			},
+			want: map[string]string{
+				"role": "team_member",
+				"org":  "56ui84736",
+				"team": "jkituy6rt",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, tt.role.RoleToMap(), "RoleToMap()")
 		})
 	}
 }

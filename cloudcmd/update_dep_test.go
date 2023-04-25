eb6352a9183b7f954195f823d5faac12b255dcd9
itachi sasuke <8012032+Keithwachira@users.noreply.github.com>
2023-04-25 17:24:48 +0300

    add more test coverage

--- cloudcmd/update_dep_test.go
+++ cloudcmd/update_dep_test.go
@@ -1,11 +1,13 @@
 package cloudcmd
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/TykTechnologies/cloud-sdk/cloud"
+	"github.com/TykTechnologies/tykctl/internal"
 )
 
 func TestHandleEnvVariables(t *testing.T) {
@@ -43,4 +45,28 @@ func TestHandleEnvVariables(t *testing.T) {
 			assert.Equal(t, tt.expectedError, err)
 		})
 	}
+}
+
+func TestHandleDeploymentDynamicVars(t *testing.T) {
+	cmd := internal.NewCmd("test").
+		WithFlagAdder(false, setValues).
+		WithFlagAdder(false, envValues).
+		WithCommands()
+	cmd.SetArgs([]string{
+		fmt.Sprintf("--set=%s", "ExtraContext.Data.EnvData.TYK_DB_EMAILBACKEND_CODE=sendgrid"),
+		fmt.Sprintf("--envVar=%s", "TYK_DB_EMAILBACKEND_ENABLEEMAILNOTIFICATIONS=true"),
+		fmt.Sprintf("--set=%s", "ExtraContext.Data.EnvData.TYK_DB_EMAILBACKEND_DEFAULTFROMEMAIL=no-reply@tyk.io"),
+		fmt.Sprintf("--envVar=%s", "TYK_DB_EMAILBACKEND_CODE=sendgri"),
+		fmt.Sprintf("--envVar=%s", "DB=tm"),
+	})
+	err := cmd.Execute()
+	if err != nil {
+		t.Fatal(err)
+	}
+	deployment := &cloud.Deployment{
+		ExtraContext: &cloud.MetaDataStore{Data: map[string]map[string]interface{}{"EnvData": {"company": "tyk", "DB": "ty"}}},
+	}
+	err = handleDeploymentDynamicVars(deployment, cmd.Flags())
+	assert.Equal(t, nil, err)
+	assert.Equal(t, &cloud.MetaDataStore{Data: map[string]map[string]interface{}{"EnvData": {"DB": "tm", "company": "tyk", "TYK_DB_EMAILBACKEND_DEFAULTFROMEMAIL": "no-reply@tyk.io", "TYK_DB_EMAILBACKEND_CODE": "sendgri", "TYK_DB_EMAILBACKEND_ENABLEEMAILNOTIFICATIONS": true}}}, deployment.ExtraContext)
 }

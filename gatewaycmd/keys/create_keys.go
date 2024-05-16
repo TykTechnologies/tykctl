46b27d255a45cedd8570107035906720f7153b45
itachi sasuke <8012032+Keithwachira@users.noreply.github.com>
2024-05-16 15:53:39 +0300

    add server secretes

--- gatewaycmd/keys/create_keys.go
+++ gatewaycmd/keys/create_keys.go
@@ -23,7 +23,10 @@ func NewCreateKeyCmd(apimClient internal.ApimClient) *cobra.Command {
 		WithFlagAdder(false, createKeyFlags).
 		AddPreRunFuncs(func(cmd *cobra.Command, args []string) error {
 			shared.AddGatewaySecret(apimClient.Client.GetConfig())
-			shared.AddGatewayServers(apimClient.Client.GetConfig())
+			err := shared.AddGatewayServers(apimClient.Client.GetConfig())
+			if err != nil {
+				return err
+			}
 			return cmd.MarkFlagRequired(file)
 		}).NoArgs(func(ctx context.Context, cmd cobra.Command) error {
 		filePath, err := cmd.Flags().GetString(file)
